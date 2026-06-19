baa28dcd00b83775a87bbbb6e747f9a71b8e4901
Dominik Majera <dominik.majera@yakoai.cz>
2026-06-19 14:38:00 +0200

    oprava zobrazení článků z CMS

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,15 +4,14 @@ import { z } from 'astro/zod';
 
 const blog = defineCollection({
   loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      pubDate: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
-      heroImage: z.optional(image()),
-      category: z.string().optional(),
-    }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    category: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
